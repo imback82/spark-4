866420425bf9cbc19747618d57467c5434b185dd
Terry Kim <yuminkim@gmail.com>
2020-03-30 12:50:04 -0700

    Support more types to convert

--- sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicbucketing/BucketingRepartitionRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicbucketing/BucketingRepartitionRDD.scala
@@ -20,17 +20,15 @@ package org.apache.spark.sql.execution.dynamicbucketing
 import scala.collection.mutable.ListBuffer
 
 import org.apache.spark.{Partition, TaskContext}
-import org.apache.spark.rdd.{CoalescedRDD, CoalescedRDDPartition, RDD}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.catalog.BucketSpec
-import org.apache.spark.sql.catalyst.expressions.{Attribute, SortOrder, UnsafeProjection}
-import org.apache.spark.sql.catalyst.plans.physical.{HashPartitioning, Partitioning}
-import org.apache.spark.sql.execution.{SparkPlan, UnaryExecNode}
+import org.apache.spark.sql.catalyst.expressions.{Attribute, UnsafeProjection}
+import org.apache.spark.sql.catalyst.plans.physical.HashPartitioning
 import org.apache.spark.sql.execution.datasources.{FilePartition, FileScanRDD, PartitionedFile}
-import org.apache.spark.sql.execution.vectorized.{MappedColumnVector, WritableColumnVector}
-import org.apache.spark.sql.types.{BinaryType, BooleanType, ByteType, DataType, DateType, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StringType, TimestampType}
-import org.apache.spark.sql.vectorized.{ColumnarBatch, ColumnVector}
+import org.apache.spark.sql.execution.vectorized.WritableColumnVector
+import org.apache.spark.sql.types.{BinaryType, BooleanType, ByteType, CalendarIntervalType, DataType, DateType, Decimal, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StringType, TimestampType}
+import org.apache.spark.sql.vectorized.ColumnarBatch
 
 private[spark] class BucketingRepartitionRDD(
     @transient private val sparkSession: SparkSession,
@@ -61,6 +59,17 @@ private[spark] class BucketingRepartitionRDD(
         getBucketId(r) == split.index
       case _ => true
     }.asInstanceOf[Iterator[InternalRow]]
+  }
+
+  private lazy val getBucketId: InternalRow => Int = {
+    val bucketIdExpression = {
+      val bucketColumns = bucketSpec.bucketColumnNames.map(
+        c => output.find(_.name == c).get)
+      HashPartitioning(bucketColumns, bucketSpec.numParallelism.get).partitionIdExpression
+    }
+
+    val projection = UnsafeProjection.create(Seq(bucketIdExpression), output)
+    row => projection(row).getInt(0)
   }
 
   private def convert(mapping: Seq[Int], col: WritableColumnVector): Unit = {
@@ -129,28 +138,67 @@ private[spark] class BucketingRepartitionRDD(
             col.putArray(i, col.getArrayOffset(mapping(i)), col.getArrayLength(mapping(i)))
           }
         }
+      case _: BinaryType =>
+        for (i <- 0 until mapping.length) {
+          if (col.isNullAt(mapping(i))) {
+            col.putNull(i)
+          } else {
+            col.putArray(i, col.getArrayOffset(mapping(i)), col.getArrayLength(mapping(i)))
+          }
+        }
+      case dt: DecimalType =>
+        if (dt.precision <= Decimal.MAX_INT_DIGITS) {
+          for (i <- 0 until mapping.length) {
+            if (col.isNullAt(mapping(i))) {
+              col.putNull(i)
+            } else {
+              col.putInt(i, col.getInt(mapping(i)))
+            }
+          }
+        } else if (dt.precision <= Decimal.MAX_LONG_DIGITS) {
+          for (i <- 0 until mapping.length) {
+            if (col.isNullAt(mapping(i))) {
+              col.putNull(i)
+            } else {
+              col.putLong(i, col.getLong(mapping(i)))
+            }
+          }
+        } else {
+          for (i <- 0 until mapping.length) {
+            if (col.isNullAt(mapping(i))) {
+              col.putNull(i)
+            } else {
+              col.putArray(i, col.getArrayOffset(mapping(i)), col.getArrayLength(mapping(i)))
+            }
+          }
+        }
+      case _: CalendarIntervalType =>
+        val months = col.getChild(0)
+        val microseconds = col.getChild(1)
+        for (i <- 0 until mapping.length) {
+          if (col.isNullAt(mapping(i))) {
+            col.putNull(i)
+          } else {
+            months.putInt(i, months.getInt(mapping(i)))
+            microseconds.putLong(i, microseconds.getLong(mapping(i)))
+          }
+        }
+      case _: DateType =>
+        for (i <- 0 until mapping.length) {
+          if (col.isNullAt(mapping(i))) {
+            col.putNull(i)
+          } else {
+            col.putInt(i, col.getInt(mapping(i)))
+          }
+        }
+      case _: TimestampType =>
+        for (i <- 0 until mapping.length) {
+          if (col.isNullAt(mapping(i))) {
+            col.putNull(i)
+          } else {
+            col.putLong(i, col.getLong(mapping(i)))
+          }
+        }
     }
-  }
-//
-//      else if (dt.isInstanceOf[BinaryType]) row.update(i, getBinary(i))
-//      else if (dt.isInstanceOf[DecimalType]) {
-//        val t = dt.asInstanceOf[DecimalType]
-//        row.setDecimal(i, getDecimal(i, t.precision, t.scale), t.precision)
-//      }
-//      else if (dt.isInstanceOf[DateType]) row.setInt(i, getInt(i))
-//      else if (dt.isInstanceOf[TimestampType]) row.setLong(i, getLong(i))
-//      else throw new RuntimeException("Not implemented. " + dt)
-//    }
-//  }
-
-  private lazy val getBucketId: InternalRow => Int = {
-    val bucketIdExpression = {
-      val bucketColumns = bucketSpec.bucketColumnNames.map(
-        c => output.find(_.name == c).get)
-      HashPartitioning(bucketColumns, bucketSpec.numParallelism.get).partitionIdExpression
-    }
-
-    val projection = UnsafeProjection.create(Seq(bucketIdExpression), output)
-    row => projection(row).getInt(0)
   }
 }
